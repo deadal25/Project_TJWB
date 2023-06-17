f879885b18d8ce0204d0bb5c26954074b6111c99
deadal25 <alqad.ri2505@gmail.com>
2023-06-17 16:17:30 +0700

    Tugas TJWB

---
+++ FINAL PROJECT TJWB/client.py
@@ -0,0 +1,91 @@
+from flask import Flask, request, render_template, redirect, send_file, make_response
+from ftplib import FTP
+from io import BytesIO
+
+app = Flask(__name__)
+
+# Konfigurasi FTP
+ftp_host = "192.168.1.13"
+ftp_user = 'alqa'
+ftp_password = 'alqa'
+
+def get_ftp_connection():
+    ftp = FTP(ftp_host)
+    ftp.login(user=ftp_user, passwd=ftp_password)
+    return ftp
+
+# Fungsi untuk mengupload file ke server FTP
+def upload_file(file):
+    try:
+        with get_ftp_connection() as ftp:
+            # Mengirim file langsung dari objek FileStorage
+            ftp.storbinary('STOR {}'.format(file.filename), file.stream)
+        
+        file_list = get_file_list()
+        file_list.append(file.filename)
+    except Exception as e:
+        # Tangani kesalahan jika terjadi
+        print(f"Error during file upload: {str(e)}")
+
+# Fungsi untuk mengunduh file dari server FTP
+def download_file(file_name):
+    try:
+        with get_ftp_connection() as ftp:
+            # Mengunduh file langsung ke objek BytesIO
+            file_data = BytesIO()
+            ftp.retrbinary('RETR {}'.format(file_name), file_data.write)
+
+            # Mengembalikan data file yang diunduh
+            file_data.seek(0)
+            return file_data.getvalue()  # Mengubah BytesIO menjadi bytes
+    except Exception as e:
+        # Tangani kesalahan jika terjadi
+        print(f"Error during file download: {str(e)}")
+
+def get_file_list():
+    try:
+        with get_ftp_connection() as ftp:
+            file_list = ftp.nlst()
+
+        return file_list
+    except Exception as e:
+        # Tangani kesalahan jika terjadi
+        print(f"Error during getting file list: {str(e)}")
+        return []
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if 'upload_file' in request.files:
+            file = request.files['upload_file']
+            if file.filename == '':
+                return render_template('index.html', error_message='Mohon pilih file yang ingin diunggah')
+            upload_file(file)
+            return redirect('/uploaded')
+        elif 'download_file' in request.form:
+            file_name = request.form['download_file']
+            if file_name == '':
+                return render_template('index.html', error_message='Mohon masukkan nama file yang ingin diunduh')
+            file_data = download_file(file_name)
+            if file_data:
+                response = make_response(file_data)
+                response.headers['Content-Disposition'] = f"attachment; filename={file_name}"
+                return response
+            else:
+                return render_template('index.html', error_message='File tidak ditemukan')
+            
+    file_list = get_file_list()
+    return render_template('index.html', file_list=file_list)
+
+@app.route('/uploaded')
+def uploaded():
+    return render_template('uploaded.html')
+
+# Route ingin melihat daftar file
+@app.route('/ftp')
+def ftp_file_list():
+    file_list = get_file_list()
+    return render_template('ftplist.html', file_list=file_list)
+
+if __name__ == '__main__':
+    app.run(debug=True)
