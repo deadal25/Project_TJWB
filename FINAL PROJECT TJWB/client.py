cfb6f3bb4ef0bbea798d47049c097aec03b4fcc5
deadal25 <alqad.ri2505@gmail.com>
2023-06-18 13:51:23 +0700

    Menambahkan tabel dan tombol download untuk setuaip daftar file ftp

--- FINAL PROJECT TJWB/client.py
+++ FINAL PROJECT TJWB/client.py
@@ -9,10 +9,10 @@ ftp_host = "192.168.1.13"
 ftp_user = 'ServerSavvy'
 ftp_password = 'Kelompok1TJW'
 
-# Set secret key for session management
+# Mengatur kunci rahasia untuk manajemen sesi
 app.secret_key = 'your_secret_key'
 
-
+#fungsi ntuk menghasilkan objek koneksi ke server FTP.
 def get_ftp_connection():
     ftp = FTP(ftp_host)
     ftp.login(user=ftp_user, passwd=ftp_password)
@@ -48,7 +48,7 @@ def download_file(file_name):
         # Tangani kesalahan jika terjadi
         print(f"Error during file download: {str(e)}")
 
-
+#fungsi untuk mendapatkan daftar file yang ada di server FTP
 def get_file_list():
     try:
         with get_ftp_connection() as ftp:
@@ -60,7 +60,7 @@ def get_file_list():
         print(f"Error during getting file list: {str(e)}")
         return []
 
-
+#rute menuju halaman login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -75,15 +75,13 @@ def login():
             return render_template('login.html', error_message='Invalid username or password')
     return render_template('login.html')
 
-
-
-# Decorator to check if the user is logged in before accessing certain routes
+# Decorator untuk memeriksa apakah pengguna telah melakukan login sebelum mengakses rute tertentu
 @app.before_request
 def require_login():
     if request.path != '/login' and 'username' not in session:
         return redirect('/login')
 
-
+#rute untuk mengupload file ke server FTP
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -93,6 +91,7 @@ def index():
                 return render_template('index.html', error_message='Mohon pilih file yang ingin diunggah')
             upload_file(file)
             return redirect('/uploaded')
+        #untuk rute download
         elif 'download_file' in request.form:
             file_name = request.form['download_file']
             if file_name == '':
@@ -108,11 +107,25 @@ def index():
     file_list = get_file_list()
     return render_template('index.html', file_list=file_list)
 
-
+#rute yang digunakan untuk menampilkan halaman yang menunjukkan pesan sukses setelah pengguna berhasil mengunggah file.
 @app.route('/uploaded')
 def uploaded():
     return render_template('uploaded.html')
 
+# Rute untuk mengunduh file dari server FTP
+@app.route('/download-file', methods=['GET'])
+def download_from_ftp():
+    file_name = request.args.get('file_name')
+    if file_name:
+        file_data = download_file(file_name)
+        if file_data:
+            response = make_response(file_data)
+            response.headers['Content-Disposition'] = f"attachment; filename={file_name}"
+            return response
+    return render_template('index.html', error_message='File tidak ditemukan')
+
+
+#rute yang digunakan untuk menghapus file dari server FTP
 @app.route('/delete-file', methods=['POST'])
 def delete_file():
     file_name = request.form.get('file_name')
@@ -125,18 +138,18 @@ def delete_file():
 
     return redirect('/ftp')
 
-# Route ingin melihat daftar file
+# Rute ingin melihat daftar file
 @app.route('/ftp')
 def ftp_file_list():
     file_list = get_file_list()
     return render_template('ftplist.html', file_list=file_list)
 
 
-@app.route('/logout')
-def logout():
-    session.pop('username', None)
-    return redirect('/login')
+# @app.route('/logout')
+# def logout():
+#     session.pop('username', None)
+#     return redirect('/login')
 
-
+#menjalankan aplikasi Flask ketika file ini dieksekusi langsung 
 if __name__ == '__main__':
     app.run(debug=True)
